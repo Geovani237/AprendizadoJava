cd0c18dec63870d26755d4c7f0c01bd0e14f1b45
Geovani237 <geovanicsouza7@gmail.com>
2026-02-18 09:51:47 -0300

    Variáveis e operadores aritméticos

---
+++ src/com/github/variaveisearitmeticos/Principal.java
@@ -0,0 +1,12 @@
+package com.github.variaveisearitmeticos;
+
+public class Principal {
+    public static void main(String[] args) {
+        int numero1 = 20;
+        int numero2 = 30;
+        int numero3 = 100;
+        int media = (numero1 + numero2 + numero3) / 3;
+
+        System.out.println("A média dos número é: " + media);
+    }
+}
