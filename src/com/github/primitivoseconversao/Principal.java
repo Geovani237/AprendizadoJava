89b06b034a6a8f36394658ea538cb24101bf399f
Geovani237 <geovanicsouza7@gmail.com>
2026-02-20 09:32:58 -0300

    Tipo Primitivo e conversão de tipo

---
+++ src/com/github/primitivoseconversao/Principal.java
@@ -0,0 +1,12 @@
+package com.github.primitivoseconversao;
+
+public class Principal {
+    public static void main(String[] args) {
+        double pesoCarga1 = 1033.49;
+        double pesoCarga2 = 849.88;
+
+        int pesoTotalCargas =  (int) (pesoCarga1 + pesoCarga2);
+
+        System.out.println("Peso total das cargas: " + pesoTotalCargas);
+    }
+}
