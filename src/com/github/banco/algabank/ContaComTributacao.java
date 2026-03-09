7736e89a3d20ad9568e7cd858b1ec6d98ac6ad85
Geovani237 <geovanicsouza7@gmail.com>
2026-03-09 14:17:22 -0300

    reduzindo acoplamento com composicao

---
+++ src/com/github/banco/algabank/ContaComTributacao.java
@@ -0,0 +1,46 @@
+package com.github.banco.algabank;
+
+import com.github.banco.javabank.Conta;
+
+import java.util.Objects;
+
+public class ContaComTributacao implements Conta {
+    public static final double TAXA_IMPOSTO_MOVIMENTACAO = 0.1;
+    private Conta contaOriginal; // Composição
+
+    public ContaComTributacao(Conta contaOriginal) {
+        Objects.requireNonNull(contaOriginal);
+        this.contaOriginal = contaOriginal;
+    }
+
+    @Override
+    public double getSaldo() {
+        return contaOriginal.getSaldo();//Delegação
+    }
+
+    @Override
+    public void sacar(double valor) {
+        contaOriginal.sacar(valor);
+        debitarImpostoMovimentacao(valor);
+    }
+
+    @Override
+    public void depositar(double valor) {
+        contaOriginal.depositar(valor);
+    }
+
+    @Override
+    public void transferir(Conta conta, double valor) {
+        contaOriginal.transferir(conta, valor);
+        debitarImpostoMovimentacao(valor);
+    }
+
+    @Override
+    public void aplicarEmInvestimento(double valor) {
+        contaOriginal.aplicarEmInvestimento(valor);
+    }
+
+    private void debitarImpostoMovimentacao(double valorMovimentacao) {
+        contaOriginal.sacar(valorMovimentacao * TAXA_IMPOSTO_MOVIMENTACAO);
+    }
+}
