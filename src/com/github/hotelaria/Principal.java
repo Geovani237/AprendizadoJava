a3992294972a4a9a32f3fbc698bdaa7b5e4de938
Geovani237 <geovanicsouza7@gmail.com>
2026-03-16 17:59:33 -0300

    Percorrendo lista com ListIterator

--- src/com/github/hotelaria/Principal.java
+++ src/com/github/hotelaria/Principal.java
@@ -5,6 +5,7 @@ import com.github.hotelaria.agencia.Hotel;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.ListIterator;
 
 public class Principal {
     public static void main(String[] args) {
@@ -16,7 +17,7 @@ public class Principal {
         cadastro.adicionar("Mercure", "Uberlândia/MG", 400);
 
 //        cadastro.adicionar("Mercure", "Uberlandia/MG", 400);
-        cadastro.removerPorCidade("Fortim/CE");
+//        cadastro.removerPorCidade("Fortim/CE");
 //        cadastro.remover(new Hotel("Vila Selvagem", "Fortim/CE", 0));
 //        cadastro.removerTodos();
 
@@ -26,12 +27,23 @@ public class Principal {
     }
 
     private static void imprimirHoteis(ArrayList<Hotel> hoteis){
-        Iterator<Hotel> hotelIterator = hoteis.iterator();
-        while (hotelIterator.hasNext()) {
-            Hotel hotel = hotelIterator.next();
+        for (Hotel hotel: hoteis) {
             System.out.printf("%s (%s) -> %.2f%n", hotel.getNome(), hotel.getCidade(), hotel.getPrecoDiaria());
-
         }
+//
+//        ListIterator<Hotel> hotelIterator = hoteis.listIterator(hoteis.size());
+//        while (hotelIterator.hasPrevious()) {
+//            Hotel hotel = hotelIterator.previous();
+//            System.out.printf("%s (%s) -> %.2f%n", hotel.getNome(), hotel.getCidade(), hotel.getPrecoDiaria());
+//        }
+
+
+
+//        Iterator<Hotel> hotelIterator = hoteis.iterator();
+//        while (hotelIterator.hasNext()) {
+//            Hotel hotel = hotelIterator.next();
+//            System.out.printf("%s (%s) -> %.2f%n", hotel.getNome(), hotel.getCidade(), hotel.getPrecoDiaria());
+//        }
 
 
 //        for (int i = 0; i < hoteis.size(); i++) {
