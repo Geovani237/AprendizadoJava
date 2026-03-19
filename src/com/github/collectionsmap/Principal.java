7a1d1a57983b0724dd3739f880068868887e69e8
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-03-19 15:05:32 -0300

    testando TreeMap e LinkedHashMap

--- src/com/github/collectionsmap/Principal.java
+++ src/com/github/collectionsmap/Principal.java
@@ -3,10 +3,7 @@ package com.github.collectionsmap;
 import com.github.collectionsmap.detran.Carro;
 import com.github.collectionsmap.detran.Proprietario;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Principal {
     public static void main(String[] args) {
@@ -21,18 +18,22 @@ public class Principal {
         Carro ford = new Carro("JAV5A67", "Ford Ka");
         Carro porsche = new Carro("EJA0V44", "Porsche 911");
 
-        Map<Carro, Proprietario> proprietarios = new HashMap<>();
+//        Map<Carro, Proprietario> proprietarios = new HashMap<>();
+//        Map<Carro, Proprietario> proprietarios = new LinkedHashMap<>();
+        Map<Carro, Proprietario> proprietarios = new TreeMap<>();
         proprietarios.put(bmw, maria);
         proprietarios.put(honda, ana);
         proprietarios.put(ford, jose);
         proprietarios.put(porsche, maria);
 
-        proprietarios.put(porsche, jose);
+//        proprietarios.put(porsche, jose);
 
 //        proprietarios.remove(new Carro("JAV5A67", "XXXXXX"));
 
-        proprietarios.put(null, joao);
-        System.out.println(proprietarios);
+//        proprietarios.put(null, joao);
+
+//        System.out.println(proprietarios);
+
 //        System.out.println(proprietarios.get(bmw));
 
 //        Set<Carro> chave = proprietarios.keySet();
@@ -56,9 +57,9 @@ public class Principal {
 //        for (Map.Entry<Carro, Proprietario> entry : proprietarios.entrySet()) {
 //            System.out.printf("%s (%s) = %s%n",entry.getKey().getPlaca(), entry.getKey().getModelo(), entry.getValue().getNome());
 //        }
-//        for (var entry : proprietarios.entrySet()) {
-//            System.out.printf("%s (%s) = %s%n",entry.getKey().getPlaca(), entry.getKey().getModelo(), entry.getValue().getNome());
-//        }
+        for (var entry : proprietarios.entrySet()) {
+            System.out.printf("%s (%s) = %s%n",entry.getKey().getPlaca(), entry.getKey().getModelo(), entry.getValue().getNome());
+        }
 
 
 
