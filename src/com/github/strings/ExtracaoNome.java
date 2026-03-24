c9bcfd2ddad49e917821dbd109fcf5f7776524e4
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-03-24 10:04:50 -0300

    Extraindo trechos da String com lastIndexOf e substring

--- src/com/github/strings/ExtracaoNome.java
+++ src/com/github/strings/ExtracaoNome.java
@@ -2,14 +2,27 @@ package com.github.strings;
 
 public class ExtracaoNome {
     public static void main(String[] args) {
-        String nome = "João Silva Souza";
+        String nome = "João Silva Souza Magalhães";
 
         // para identificar o 1 espaço utiliza indexOf
 //        System.out.println(nome.indexOf(" ")); //4
 //        System.out.println(nome.substring(5));
 //        System.out.println(nome.substring(5, 10));
-        System.out.println(obterSegundonome(nome));
+//
+//        System.out.println(nome.lastIndexOf(" "));
+//        System.out.println(nome.substring(11));
+//        System.out.println(obterSegundonome(nome));
+        System.out.println(obterUltimoNome(nome));
     }
+
+    private static String obterUltimoNome(String nome) {
+        int posicaoUltimoEspaco = nome.lastIndexOf(" "); //Se o caracter não é econtrado com o IndexOf ele retorna um número negativo
+        if (posicaoUltimoEspaco < 0) {
+            throw new RuntimeException("Não é um nome completo");
+        }
+        return nome.substring(posicaoUltimoEspaco + 1);
+    }
+
 
     private static String obterSobrenome(String nome) {
         int posicaoPrimeiroEspaco = nome.indexOf(" "); //Se o caracter não é econtrado com o IndexOf ele retorna um número negativo
