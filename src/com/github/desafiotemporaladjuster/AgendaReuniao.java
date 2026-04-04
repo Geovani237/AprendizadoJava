8feb2d23706e0f5ba5d8fe72bce3d7ce9d51ef72
Geovani237 <geovanicsouza7@gmail.com>
2026-04-03 22:36:36 -0300

    Temporal Adjuster

--- src/com/github/desafiotemporaladjuster/AgendaReuniao.java
+++ src/com/github/desafiotemporaladjuster/AgendaReuniao.java
@@ -1,30 +1,19 @@
 package com.github.desafiotemporaladjuster;
 
-import java.time.DayOfWeek;
-import java.time.Month;
-import java.time.Year;
+import java.time.*;
 import java.time.format.TextStyle;
+import java.time.temporal.TemporalAdjusters;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class AgendaReuniao {
-    //Pré requisitos
-    //Year -> pedir o Ano
-    //Month -> pedir o mes
-    //MonthYear -> provavelmente unir Year e Month
-    //Fim pré requisitos
-    //sera usado o Enum DayOfWeek
-    //provalvelmente MonthDay
-    //Reuniões toda a primeira segunda-feira do mês
-
-
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Ano: ");
         String anoReuniaoTexto = scanner.nextLine();
         Year anoReuniao = Year.parse(anoReuniaoTexto);
+        int anoReuniaoInt = anoReuniao.getValue();
 
         System.out.print("Mês inicial: ");
         int mesInicialInt = scanner.nextInt();
@@ -32,15 +21,17 @@ public class AgendaReuniao {
         System.out.println("Gerando agenda anual de reunião desde " +
                 mesInicial.getDisplayName(TextStyle.FULL, new Locale("pt", "BR")) + " de " + anoReuniao);
 
-        DayOfWeek diaDaReuniao = DayOfWeek.MONDAY;
-        System.out.println(diaDaReuniao);
+        DayOfWeek diaDaReuniao = DayOfWeek.from(DayOfWeek.MONDAY);
+
+        LocalDate data = LocalDate.of(anoReuniaoInt,mesInicial,1);
         while (mesInicialInt <= 12) {
-            System.out.println(mesInicial.getDisplayName(TextStyle.FULL, new Locale("pt", "BR")) + " de " + anoReuniao);
+
+            LocalDate segundaMesAtual = data.with(TemporalAdjusters.firstInMonth(DayOfWeek.MONDAY));
+            System.out.println(diaDaReuniao.getDisplayName(TextStyle.FULL, new Locale("pt", "BR")) + ", " + segundaMesAtual.getDayOfMonth()  + " de " + mesInicial.getDisplayName(TextStyle.FULL, new Locale("pt", "BR")) + " de " + anoReuniao);
+
             mesInicial = mesInicial.plus(1);
+            data = data.plusMonths(1);
             mesInicialInt++;
         }
     }
-
-
-
 }
