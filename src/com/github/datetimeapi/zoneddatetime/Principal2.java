b6dc1d9fa6cbd7b8e0532765e6b1dcf215e59fed
Geovani237 <geovanicsouza7@gmail.com>
2026-04-04 17:25:13 -0300

    ZonedDateTime data/hora com fuso horário

---
+++ src/com/github/datetimeapi/zoneddatetime/Principal2.java
@@ -0,0 +1,18 @@
+package com.github.datetimeapi.zoneddatetime;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public class Principal2 {
+    public static void main(String[] args) {
+        ZonedDateTime dataFestaSaoPaulo = ZonedDateTime.parse("2023-09-13T20:00:00-03:00[America/Sao_Paulo]");
+//        ZonedDateTime dataFestaLosAngeles = dataFestaSaoPaulo.withZoneSameLocal(ZoneId.of("America/Los_Angeles"));//withZoneSameLocal trava a hora local
+        ZonedDateTime dataFestaLosAngeles = dataFestaSaoPaulo.withZoneSameInstant(ZoneId.of("America/Los_Angeles"));
+
+        System.out.println(dataFestaSaoPaulo);
+        System.out.println(dataFestaLosAngeles);
+
+        System.out.println(dataFestaSaoPaulo.equals(dataFestaLosAngeles));//não compara o instante específico
+        System.out.println(dataFestaSaoPaulo.isEqual(dataFestaLosAngeles));//Compara o instante específico
+    }
+}
