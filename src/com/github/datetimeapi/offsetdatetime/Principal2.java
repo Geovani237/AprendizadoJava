245280a669e79f7d7a0cacba2e4df0f9a0fb148c
Geovani237 <geovanicsouza7@gmail.com>
2026-04-06 08:39:04 -0300

    OffsetDateTime e OffsetTime: data e hora com deslocamento do UTC

---
+++ src/com/github/datetimeapi/offsetdatetime/Principal2.java
@@ -0,0 +1,10 @@
+package com.github.datetimeapi.offsetdatetime;
+
+import java.time.OffsetTime;
+
+public class Principal2 {
+    public static void main(String[] args) {
+        OffsetTime hora = OffsetTime.now();
+        System.out.println(hora);
+    }
+}
