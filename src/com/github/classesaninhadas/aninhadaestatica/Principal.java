fc08031b8fd688ad77ae914d1e627beaa65e2bc0
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-04-07 09:35:25 -0300

    Refatorando o código para fazer com que seja Aninhado estático

--- src/com/github/classesaninhadas/aninhadaestatica/Principal.java
+++ src/com/github/classesaninhadas/aninhadaestatica/Principal.java
@@ -3,7 +3,7 @@ package com.github.classesaninhadas.aninhadaestatica;
 public class Principal {
     public static void main(String[] args) {
         ServicoEmail servico = new ServicoEmail();
-        Mensagem mensagem = new Mensagem("joao@mail.com", "maria@mail.com", "Olá, tudo bem?");
+        ServicoEmail.Mensagem mensagem = new ServicoEmail.Mensagem("joao@mail.com", "maria@mail.com", "Olá, tudo bem?");
         servico.enviar(mensagem);
     }
 }
--- src/com/github/classesaninhadas/aninhadaestatica/ServicoEmail.java
+++ src/com/github/classesaninhadas/aninhadaestatica/ServicoEmail.java
@@ -5,4 +5,35 @@ public class ServicoEmail {
     public void enviar(Mensagem mensagem) {
         System.out.printf("Enviando de %s para %s: %s%n", mensagem.getRemetente(), mensagem.getDestinatario(), mensagem.getTexto());
     }
+
+    private static String gerarAssinatura() {
+        return "\n--\nGeovani-Java";
+    }
+
+    public static class Mensagem {
+        private final String remetente;
+        private final String destinatario;
+        private final String texto;
+
+        public Mensagem(String remetente, String destinatario, String texto) {
+            this.remetente = remetente;
+            this.destinatario = destinatario;
+            this.texto = texto;
+        }
+
+        public String getRemetente() {
+            return remetente;
+        }
+
+        public String getDestinatario() {
+            return destinatario;
+        }
+
+        public String getTexto() {
+            return texto + gerarAssinatura();
+        }
+    }
+
 }
+
+// Uma boa para pensar antes de criar classe aninhada, uma classe aninhada só deve existir para servir a sua classe superior.
