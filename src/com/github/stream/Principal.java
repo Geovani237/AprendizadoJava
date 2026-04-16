10f711ace9491182d38ce408b5b97572939b1d64
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-04-16 15:35:38 -0300

    Achatando um Stream com Stream.flatMap

--- src/com/github/stream/Principal.java
+++ src/com/github/stream/Principal.java
@@ -1,12 +1,14 @@
 package com.github.stream;
 
 import com.github.stream.estoque.CadastroProduto;
+import com.github.stream.estoque.Categoria;
 import com.github.stream.estoque.Fabricante;
 import com.github.stream.estoque.Produto;
 
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Stream;
 
 public class Principal {
@@ -16,8 +18,12 @@ public class Principal {
 
         produtos.stream()
                 .filter(Produto::temEstoque)
-                .map(Produto::getFabricante)
-                .distinct() //Operação intermediária com estada (stateful) pode precisar processar todos os elementos do Stream, retorna uma stream com elementos únicos, e tem de ter o metodo equals implementado na classe,
+                .flatMap(produto -> produto.getCategorias().stream())
+                .distinct()
                 .forEach(System.out::println);
+
+//        stream.forEach(obj -> {
+//            System.out.println(obj.getClass().getName() + " - " + obj);
+//        });
     }
 }
