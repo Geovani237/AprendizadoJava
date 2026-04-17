4e627041316b943bd9a6bdf609b945c1ea1eb0e5
Geovani237 <geovanicsouza7@gmail.com>
2026-04-17 09:13:45 -0300

    Operações de redução especiais: sum, average e count

--- src/com/github/stream/Principal.java
+++ src/com/github/stream/Principal.java
@@ -16,18 +16,26 @@ public class Principal {
     public static void main(String[] args) {
         var cadastroProduto = new CadastroProduto();
         List<Produto> produtos = cadastroProduto.obterTodos();
-//        List<Produto> produtos = new ArrayList<>();
 
-        OptionalInt maiorQuantidadeOptional = produtos.stream()
-                .mapToInt(Produto::getQuantidade)
-                .reduce(Integer::max);
-//                .reduce(0, Integer::max);
+//        int totalEstoque = produtos.stream()
+//                .mapToInt(Produto::getQuantidade)
+//                .sum();
+////                .reduce(0, Integer::sum);
+//
+//        System.out.println(totalEstoque);
 
-        int maiorQuantidade = maiorQuantidadeOptional
-                .orElseThrow(() -> new RuntimeException("Quantidade náo encontrada"));
+//        double mediaEstoque = produtos.stream()
+//                .mapToInt(Produto::getQuantidade)
+//                .average()
+//                .orElseThrow(() -> new RuntimeException("Não há produtos para calcular a média"));
+//
+//        System.out.println(mediaEstoque);
 
-        System.out.println(maiorQuantidade);
+        long totalProdutosComEstoque = produtos.stream()
+                .filter(Produto::temEstoque)
+                .count();
 
+        System.out.println(totalProdutosComEstoque);
     }
 
 
