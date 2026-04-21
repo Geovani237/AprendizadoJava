c2c9655940c01cbde064db3abcff3f6eb6c48f42
Geovani237 <geovanicsouza7@gmail.com>
2026-04-21 16:36:39 -0300

    Gerando mapas particionados com Collectors.partitioningBy

--- src/com/github/stream/Principal.java
+++ src/com/github/stream/Principal.java
@@ -19,20 +19,15 @@ public class Principal {
         var cadastroProduto = new CadastroProduto();
         List<Produto> produtos = cadastroProduto.obterTodos();
 
-//        Map<String, Long> produtosPorFabricante = produtos.stream()
-//                .filter(Produto::temEstoque)
-//                .collect(Collectors.groupingBy(produto -> produto.getFabricante().nome(),
-//                        Collectors.counting()));
-//
-//        System.out.println(produtosPorFabricante);
+        Map<Boolean, List<Produto>> estoqueProdutos = produtos.stream()
+                .collect(Collectors.partitioningBy(Produto::temEstoque));
 
-        Map<String, Integer> estoquePorFabricante = produtos.stream()
-                .filter(Produto::temEstoque)
-                .collect(Collectors.groupingBy(produto -> produto.getFabricante().nome(),
-                        Collectors.summingInt(Produto::getQuantidade)));
-
-        System.out.println(estoquePorFabricante);
-
+        estoqueProdutos.forEach((chave, valor) -> {
+            System.out.println();
+            System.out.println(chave);
+            System.out.println("--------");
+            valor.forEach(System.out::println);
+        });
     }
 
 
