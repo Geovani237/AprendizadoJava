449c4377ba5dcda88e3ccb0ad8b80c1945e3d3bd
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-04-22 14:31:51 -0300

    Listando arquivos e diretórios

---
+++ src/com/github/classicoio/Principal4.java
@@ -0,0 +1,22 @@
+package com.github.classicoio;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Principal4 {
+    public static void main(String[] args) throws IOException {
+        File pasta = new File("docs").getCanonicalFile();
+
+//        File[] arquivos = pasta.listFiles();
+//        File[] arquivos = pasta.listFiles(File::isFile);
+//        File[] arquivos = pasta.listFiles(File::isDirectory);
+        File[] arquivos = pasta.listFiles((dir, nome) -> nome.startsWith("c"));
+
+        if (arquivos != null) {
+            for (File arquivo : arquivos) {
+                System.out.println(arquivo.getName());
+            }
+        }
+
+    }
+}
