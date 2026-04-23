2b08237ab50c604c2b8d3c0a8601524c43ac6454
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-04-23 16:26:15 -0300

    Começando o pensamento para desenvolver o desafio

--- src/com/github/desafios/desafioclassicoio/io/FragmentadorDeArquivo.java
+++ src/com/github/desafios/desafioclassicoio/io/FragmentadorDeArquivo.java
@@ -1,4 +1,18 @@
 package com.github.desafios.desafioclassicoio.io;
 
+import java.io.File;
+
 public class FragmentadorDeArquivo {
+    File nomeDaPasta;
+    int tamanho;
+
+    public FragmentadorDeArquivo(File nomeDaPasta, int tamanho) {
+        this.nomeDaPasta = nomeDaPasta;
+        this.tamanho = tamanho;
+    }
+
+    public void fragmentar() {
+        System.out.println(nomeDaPasta.length());
+        System.out.println(nomeDaPasta.getParent());
+    }
 }
