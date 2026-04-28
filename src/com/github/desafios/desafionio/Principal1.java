c99e4b535de4feadacc768745d492c09791b6802
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-04-28 14:35:32 -0300

    Desafio NIO

---
+++ src/com/github/desafios/desafionio/Principal1.java
@@ -0,0 +1,19 @@
+package com.github.desafios.desafionio;
+
+import com.github.desafios.desafionio.nio.FragmentadorDeArquivo;
+
+import java.io.File;
+
+public class Principal1 {
+    public static void main(String[] args) {
+//        try {
+//            var fragmentador = new FragmentadorDeArquivo(
+//                    new File("arquivos/fotos/picanha.jpg"),
+//                    1024 * 50);
+//            fragmentador.fragmentar();
+//        } catch (IOException e) {
+//            System.out.println("Erro fragmentando arquivo");
+//            e.printStackTrace();
+//        }
+    }
+}
---
+++ src/com/github/desafios/desafionio/Principal2.java
@@ -0,0 +1,22 @@
+package com.github.desafios.desafionio;
+
+import com.github.desafios.desafionio.nio.UnificadorDeArquivos;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Principal2 {
+
+    public static void main(String[] args) {
+//        try {
+//            var unificador = new UnificadorDeArquivos(
+//                    new File("arquivos/fotos"),
+//                    new File("arquivos/fotos/picanha-unificado.jpg"),
+//                    "picanha.jpg");
+//            unificador.unificar();
+//        } catch (IOException e) {
+//            System.out.println("Erro ao unificar arquivos");
+//            e.printStackTrace();
+//        }
+    }
+}
