b490f6f9dc6cb6e515747a4a6e5a96ff47b5a265
Geovani Carlos de Souza <geovani.souza@colaborador.embrapa.br>
2026-04-29 09:18:12 -0300

    Serializando objetos com ObjectOutputStream

---
+++ src/com/github/serializacao/serializar/Funcionario.java
@@ -0,0 +1,65 @@
+package com.github.serializacao.serializar;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Funcionario implements Serializable {
+
+    private String nome;
+    private LocalDate dataNascimento;
+    private BigDecimal salario;
+
+    public Funcionario(String nome, LocalDate dataNascimento, BigDecimal salario) {
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.salario = salario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public BigDecimal getSalario() {
+        return salario;
+    }
+
+    public void setSalario(BigDecimal salario) {
+        this.salario = salario;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Funcionario that = (Funcionario) o;
+        return Objects.equals(nome, that.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(nome);
+    }
+
+    @Override
+    public String toString() {
+        return "Funcionario{" +
+                "nome='" + nome + '\'' +
+                ", dataNascimento=" + dataNascimento +
+                ", salario=" + salario +
+                '}';
+    }
+}
